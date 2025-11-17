92003357406b0fab8d4a78917fe930d1c0a96a30
varodv <varodv@gmail.com>
2025-11-18 00:28:13 +0100

    feat(data-table): Implement basic log data table

---
+++ app/components/log-data-table.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import type { ColumnDef } from '@tanstack/vue-table';
+import type { HTMLAttributes } from 'vue';
+import type { Activity, Log } from '../types';
+
+const props = defineProps<{
+  data: Array<Log>;
+  class?: HTMLAttributes['class'];
+}>();
+
+const { data: activityData } = useActivity();
+
+const columns: Array<ColumnDef<Log>> = [
+  {
+    accessorKey: 'activity',
+    header: () =>
+      h('span', { class: 'grow text-center truncate' }, 'Activity'),
+    cell: ({ getValue }: any) =>
+      h(
+        'span',
+        { class: 'grow text-center truncate' },
+        getActivityName(getValue()),
+      ),
+  },
+  {
+    accessorKey: 'timestamp',
+    header: () => h('span', { class: 'grow text-center truncate' }, 'Time'),
+    cell: ({ getValue }: any) =>
+      h(
+        'span',
+        { class: 'grow text-center truncate' },
+        new Date(getValue()).toLocaleTimeString(),
+      ),
+  },
+];
+
+function getActivityName(activityId: Activity['id']) {
+  return activityData.value.find(activity => activity.id === activityId)?.name ?? activityId;
+}
+</script>
+
+<template>
+  <DataTable :class="props.class" :columns="columns" :data="data">
+    <template #empty>
+      <UiTableEmpty :colspan="columns.length">
+        <slot name="empty-content">
+          No logs.
+        </slot>
+      </UiTableEmpty>
+    </template>
+  </DataTable>
+</template>
