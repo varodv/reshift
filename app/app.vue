de9e402cc98ee99c83f073a8d599dc55ae90a55a
varodv <varodv@gmail.com>
2025-11-20 23:20:08 +0100

    fix(app): Avoid zoom (2)

--- app/app.vue
+++ app/app.vue
@@ -8,7 +8,7 @@ useHead({
   meta: [
     {
       name: 'viewport',
-      content: 'width=device-width, initial-scale=1, maximum-scale=1',
+      content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0',
     },
   ],
 });
